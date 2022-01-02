263a164bece61125e869303df746be8580a06793
Daniel <ejedenials@gmail.com>
2022-01-02 04:22:21 +0100

    Update apps.py

--- newsaggregator/hackernews_engine/apps.py
+++ newsaggregator/hackernews_engine/apps.py
@@ -4,8 +4,8 @@ from django.apps import AppConfig
 class HackernewsEngineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hackernews_engine'
-    # def ready(self):
-    #     """This method is called when the application starts running"""
-    #     print("Starting Scheduler...")
-    #     from hackernews_engine.scheduler import hnapi_scheduler 
-    #     hnapi_scheduler.start()
+    def ready(self):
+        """This method is called when the application starts running"""
+        print("Starting Scheduler...")
+        from hackernews_engine.scheduler import hnapi_scheduler 
+        hnapi_scheduler.start()
